5af0d296c8f3e745639d80960ca4eb7cf51ebccd
Volkov Daniil <volkov27daniil@yandex.ru>
2022-10-18 21:44:54 +0300

    Create Post entity

---
+++ src/main/java/me/volkovd/blog/entities/Post.java
@@ -0,0 +1,47 @@
+package me.volkovd.blog.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String text;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", text='" + text + '\'' +
+                '}';
+    }
+
+}
